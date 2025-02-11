f744e8240b11d814cdbe30c8c646a456d2e8173d
lucas araujo <lucasao744@gmail.com>
2025-02-11 00:33:28 -0300

    arrow back

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { NavContainer } from "./styled";
+import { FaArrowLeft } from "react-icons/fa";
 
 export default function NavBar() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
     return (
         <NavContainer>
             <Link to="/">
-            CINEFLEX
+                <div>
+                    {location.pathname !== "/" && (
+                        <FaArrowLeft
+                            color="black"
+                            onClick={() => navigate(-1)}
+                            style={{ cursor: "pointer" }}
+                        />
+                    )}
+                </div>
+                <p>CINEFLEX</p>
             </Link>
         </NavContainer>
-    )
+    );
 }
--- src/components/NavBar/styled.js
+++ src/components/NavBar/styled.js
@@ -1,20 +1,42 @@
 import styled from "styled-components";
 
-
 export const NavContainer = styled.div`
     width: 100%;
     height: 70px;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
+    padding: 0 20px;
     background-color: #C3CFD9;
     color: #E8833A;
     font-family: 'Roboto', sans-serif;
     font-size: 34px;
     position: fixed;
     top: 0;
+    z-index: 10;
+
     a {
+        display: flex;
+        align-items: center;
         text-decoration: none;
         color: #E8833A;
+        width: 100%; 
+        justify-content: space-between; 
     }
-`
+
+    div {
+        display: flex;
+        align-items: center;
+    }
+
+    p {
+        flex: 1;
+        text-align: center;
+        margin: 0;
+    }
+
+    svg {
+        cursor: pointer;
+        font-size: 30px;
+    }
+`;
